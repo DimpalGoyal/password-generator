14de5f117b09a97896b91d53aa07f3aece7ff818
Dimpal Goyal <dimpalgoyal129@gmail.com>
2025-11-02 14:11:50 +0000

    added passwd generator

---
+++ README.md
@@ -0,0 +1,4 @@
+The password generator lets you generate a random password
+according to your preferance. It's ask you wheather you want a 
+special character or not, what should be the length but it cannot
+be less than 4 characters.
---
+++ password_gen.py
@@ -0,0 +1,51 @@
+# collect user preferences
+# length
+# should contain uppercase
+# should contain special character
+# should contain digits
+
+# get all available characters
+# randomly pick characters upto length
+# ensure we have at least one of each character type 
+# ensure length is valid
+
+import random
+import string
+
+def password_gen():
+    length = int(input("enter the desired password length: ").strip().lower())
+    include_uppercase = input("include uppercase letters? (yes/no): ").strip().lower()
+    include_special = input("include special characters? (yes/no): ").strip().lower()
+    include_digits = input("include digits? (yes/no): ").strip().lower()        
+
+    if length < 4:
+        print("password length must be at least 4 characters")
+        return
+    
+    lower = string.ascii_lowercase
+    uppercase = string.ascii_uppercase if include_uppercase == "yes" else ""
+    special = string.punctuation if include_special == "yes" else ""
+    digits = string.digits if include_digits == "yes" else ""
+    all_characters = lower + uppercase + special + digits
+
+    required_characters = []
+    if include_uppercase == "yes":
+        required_characters.append(random.choice(uppercase))
+    if include_special == "yes":
+        required_characters.append(random.choice(special))
+    if include_digits == "yes":
+        required_characters.append(random.choice(digits))
+
+    remaining_length = length - len(required_characters)        
+    password = required_characters
+
+    for _ in range(remaining_length):
+        character = random.choice(all_characters) 
+        password.append(character)
+
+    random.shuffle(password)
+    str_password = "".join(password)
+    return str_password
+
+password = password_gen()
+print(password) 
